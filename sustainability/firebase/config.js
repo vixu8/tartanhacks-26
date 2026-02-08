5b9eefd897887b7c2d2dcbfd73594b45973c4c22
vixu8 <viola.r.xu@gmail.com>
2026-02-07 19:22:33 -0500

    hide api

--- sustainability/firebase/config.js
+++ sustainability/firebase/config.js
@@ -4,8 +4,8 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCh2w43Ijcc-Ee8wbHCT-03GYOkt_Cn2yo",
-  authDomain: "sustainability-e6011.firebaseapp.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: "sustainability-e6011",
   storageBucket: "sustainability-e6011.firebasestorage.app",
   messagingSenderId: "355407033623",
